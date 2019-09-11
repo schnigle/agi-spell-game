61c93c03ff4c4dba58904739da6619b11bbafa26
Tomas Andersson <tomas3@kth.se>
2019-09-11 17:35:52 +0200

    added support for "ragdolling" enemies

--- Assets/Scripts/Combat/AITester.cs
+++ Assets/Scripts/Combat/AITester.cs
@@ -20,22 +20,40 @@ public class AITester : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        // Order AI to move to position
+        if (Input.GetMouseButtonDown(1) && enemy.agent.enabled)
         {
-            var mousePosition = Input.mousePosition;
-            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
+            Vector3 groundHit;
+            if (CursorToGroundHit(out groundHit))
             {
-                Vector3 targetPosition = hit.point;
-                // enemy.IndirectlyMoveTowards(targetPosition);
-                enemy.Agent.destination = targetPosition;
+                enemy.agent.destination = groundHit;
             }
         }
-
+        // Toggle ragdoll
+        if (Input.GetKeyDown("r"))
+        {
+            enemy.isRagdolling = !enemy.isRagdolling;
+            // enemy.body.AddForce(new Vector3(Random.Range(-5,5),15,Random.Range(-5,5)), ForceMode.VelocityChange);
+            // enemy.body.AddTorque(new Vector3(Random.Range(-50,50), Random.Range(-50,50), Random.Range(-50,50)), ForceMode.VelocityChange);
+        }
+        // Spawn a projectile that falls down on the enemy
         if (Input.GetKeyDown("p"))
         {
             Instantiate(projectile, enemy.transform.position + Vector3.up * 10, new Quaternion());
         }
+    }
+
+    bool CursorToGroundHit(out Vector3 groundHit)
+    {
+        var mousePosition = Input.mousePosition;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            groundHit = hit.point;
+            return true;
+        }
+        groundHit = new Vector3();
+        return false;
     }
 }
--- Assets/Scripts/Combat/EnemyAI.cs
+++ Assets/Scripts/Combat/EnemyAI.cs
@@ -6,22 +6,69 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
 
-    public NavMeshAgent Agent { get; private set; }
+    public NavMeshAgent agent { get; private set; }
+    public Rigidbody body { get; private set; }
+    /// True if the actor is currently trying to get up (and return to NavMesh) after being a ragdoll
+    public bool isRising { get; private set; }
+    Vector3 closestNavMeshRisePoint;
+    private bool _isRagdolling;
+    /// True if the actor is currently in "ragdoll" mode (which means that it acts as a non-kinematic rigidbody while having its movement disabled)
+    public bool isRagdolling
+    {
+        get { return _isRagdolling; }
+        set
+        {
+            if (value)
+            {
+                _isRagdolling = true;
+                agent.enabled = false;
+                body.isKinematic = false;
+                isRising = false;
+            }
+            else
+            {
+                _isRagdolling = false;
+                body.isKinematic = true;
+                isRising = true;
+                NavMeshHit hit;
+                if (NavMesh.SamplePosition(transform.position, out hit, 100, NavMesh.AllAreas))
+                {
+                    closestNavMeshRisePoint = hit.position;
+                }
+            }
+        }
+    }
+
 
     void Awake()
     {
-        Agent = GetComponent<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();
+        body = GetComponent<Rigidbody>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        isRagdolling = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        // Before resuming NavMeshAgent, return to the default angle and get back to the NavMesh.
+        if (isRising)
+        {
+            var angleRate = Time.deltaTime * 300;
+            var rotX = Mathf.MoveTowardsAngle(transform.rotation.eulerAngles.x, 0, angleRate);
+            var rotZ = Mathf.MoveTowardsAngle(transform.rotation.eulerAngles.z, 0, angleRate);
+            transform.eulerAngles = new Vector3(rotX, transform.eulerAngles.y, rotZ);
+            transform.position = Vector3.MoveTowards(transform.position, closestNavMeshRisePoint, Time.deltaTime * 15);
+            if (transform.rotation.eulerAngles.x == 0 && transform.rotation.eulerAngles.z == 0 && Vector3.SqrMagnitude(transform.position - closestNavMeshRisePoint) == 0)
+            {
+                isRising = false;
+                agent.enabled = true;
+            }
+        }
     }
 
     /// Experimental movement script. Causes the actor to move to a random point within a circle
@@ -34,7 +81,7 @@ public class EnemyAI : MonoBehaviour
         float theta = Random.Range(0, 2*Mathf.PI);
         targetPoint.x += r * Mathf.Cos(theta);
         targetPoint.z += r * Mathf.Sin(theta);
-        Agent.destination = targetPoint;
+        agent.destination = targetPoint;
     }
 
 }
