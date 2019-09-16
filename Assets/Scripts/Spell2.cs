2cabced5dcd5b2a3c36a3db27abd3b4f92a74b15
diegocc <diegocc@kth.se>
2019-09-16 12:11:38 +0200

    lev works

---
+++ Assets/Scripts/Spell2.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spell2 : MonoBehaviour
+{
+
+    private float timer = 0.0f;
+    private const float waitTime = 10.0f;
+
+    public GameObject bullet, bulletEmitter;
+    public Transform playerTrans;
+
+    public float forwardForce = 15.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("up"))
+        {
+            UnleashSpell();
+        }
+
+        timer += Time.deltaTime;
+    }
+
+    public void UnleashSpell()
+    {
+        GameObject tempBull;
+        tempBull = Instantiate(bullet, bulletEmitter.transform.forward.normalized * 0.5f + bulletEmitter.transform.position, playerTrans.rotation) as GameObject;
+        //tempBull.transform.Rotate(Vector3.left * 90);
+        Rigidbody tempBody;
+        tempBody = tempBull.GetComponent<Rigidbody>();
+        // tempBody.transform.position = (bulletEmitter.transform.forward.normalized * 2+ tempBody.position);
+        tempBody.AddForce(bulletEmitter.transform.forward * forwardForce);
+        Destroy(tempBull, waitTime);
+    }
+
+}
---
+++ Assets/Scripts/SpellLogic2.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellLogic2 : MonoBehaviour
+{
+
+    public GameObject muzzlePrefab, hitPrefab;
+    private GameObject latesthitObject;
+
+    void Start()
+    {
+        if (muzzlePrefab != null)
+        {
+            var muzzleVFX = Instantiate(muzzlePrefab, transform.position, Quaternion.identity);
+            muzzleVFX.transform.forward = gameObject.transform.forward;
+
+            var psMuzzle = muzzleVFX.GetComponent<ParticleSystem>();
+            if(psMuzzle != null)
+            {
+                Destroy(muzzleVFX, psMuzzle.main.duration);
+            }
+            else
+            {
+                var psChild = muzzleVFX.transform.GetChild(0).GetComponent<ParticleSystem>();
+                Destroy(muzzleVFX, psChild.main.duration);
+            }
+
+        }
+
+        var colliderToIgnore = GameObject.Find("PlayerObject").GetComponent<Collider>(); // traversing hierarchy like a tree
+        Physics.IgnoreCollision(colliderToIgnore, GetComponent<Collider>());
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+
+        ContactPoint contact = col.contacts[0];
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 pos = contact.point;
+
+
+          latesthitObject = col.gameObject;
+          latesthitObject.GetComponent<FloatLogic>().enabled = true;
+
+        
+
+        if(hitPrefab != null)
+        {
+            var hitVFX = Instantiate(hitPrefab, pos, rot);
+            var psHit = hitVFX.GetComponent<ParticleSystem>();
+            if(psHit != null)
+            {
+                Destroy(hitVFX, psHit.main.duration);
+            }
+            else
+            {
+                var psChild = hitVFX.transform.GetChild(0).GetComponent<ParticleSystem>();
+                Destroy(hitVFX, psChild.main.duration);
+            }
+        }
+
+      /*  var nearby = Physics.OverlapSphere(pos, 5);
+        foreach(var item in nearby)
+        {
+            Rigidbody rigidbody = null;
+            if(item.tag == "Actor")
+            {
+                //print("Death good");
+                var enemy = item.GetComponent<EnemyAI>();
+                enemy.isRagdolling = true;
+            }
+            rigidbody = item.GetComponent<Rigidbody>();
+            if (rigidbody != null)
+            {
+                var direction = (rigidbody.transform.position - pos).normalized;
+                rigidbody.AddForce((5 - Vector3.Distance(rigidbody.position, pos)) * (direction) * 200, ForceMode.Impulse);
+            }
+        }
+
+        Destroy(gameObject);*/
+    }
+}
