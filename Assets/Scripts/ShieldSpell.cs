2e34cf803f8b9657084ba0b3dae5e7a4ca172929
Tomas Andersson <tomas3@kth.se>
2019-09-25 13:25:26 +0200

    added scepter model and shield spell

---
+++ Assets/Scripts/ShieldSpell.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldSpell : MonoBehaviour, ISpell
+{
+    [SerializeField]
+    GameObject shieldPrefab = null;
+    GameObject instantiatedShield = null;
+    [SerializeField]
+    Transform staffKnob = null;
+    [SerializeField]
+    float shieldDistance = 2;
+    bool aimingShield = false;
+
+	public GestureRecognition.Gesture SpellGesture => GestureRecognition.Gesture.circle_ccw;
+
+	public void OnAimEnd()
+	{
+        Destroy(instantiatedShield, 10);
+        aimingShield = false;
+		instantiatedShield = null;
+	}
+
+	public void OnAimStart()
+	{
+		if (instantiatedShield)
+        {
+            Destroy(instantiatedShield);
+            instantiatedShield = null;
+        }
+        if (staffKnob)
+        {
+            aimingShield = true;
+            instantiatedShield = Instantiate(shieldPrefab);
+            UpdateShieldPosition();
+        }
+	}
+
+    void UpdateShieldPosition()
+    {
+        if (staffKnob != null && instantiatedShield != null)
+        {
+            instantiatedShield.transform.rotation = staffKnob.rotation;
+            instantiatedShield.transform.position = staffKnob.position + staffKnob.forward * shieldDistance;
+        }
+    }
+
+	public void UnleashSpell()
+	{
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (aimingShield)
+        {
+            UpdateShieldPosition();
+        }
+    }
+}
