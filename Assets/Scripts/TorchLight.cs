38a7fb7f83aba72c0dd8c68144e8720edb391987
Tomas Andersson <tomas3@kth.se>
2019-10-17 20:26:15 +0200

    added castle indoors props and materials

---
+++ Assets/Scripts/TorchLight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchLight : MonoBehaviour
+{
+    [SerializeField]
+    float intensityFlickerStrength = 1;
+    [SerializeField]
+    float intensityFlickerFrequency = 1;
+    new Light light;
+    float defaultIntensity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+        defaultIntensity = light.intensity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        light.intensity = defaultIntensity + Mathf.Sin(Time.time * intensityFlickerFrequency) * intensityFlickerStrength + Mathf.Sin(Time.time * intensityFlickerFrequency / 3) * intensityFlickerStrength;
+    }
+}
